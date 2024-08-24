a7e35a224901e6612a29e55bd4fab71ff877f779
it-AVNG <it.avng@gmail.com>
2024-08-24 15:56:47 +0700

    update entry for token services

--- app/api/entry.py
+++ app/api/entry.py
@@ -1,17 +1,19 @@
 import sys
 import os
+import logging
 from typing import Annotated
 from fastapi import Depends, FastAPI
-from fastapi.security import OAuth2PasswordBearer
 
 sys.path.append(f'{os.getcwd()}')
 from app.logs.log_setup import get_log_config,log
+from app.api.service import get_current_user
+from app.api.schemas import User
 
 
 get_log_config()
 
 app = FastAPI()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 @app.get("/")
@@ -24,6 +26,14 @@ def health_check():
     message()
     return {'Hello': 'world'}
 
-@app.get("/items/")
-async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
-    return {"token": token}
+# @app.get("/items/")
+# async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+#     return {"token": token}
+
+@app.get("/users/me")
+async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
+    func_logger = logging.getLogger(f'app.entry.{__name__}')
+    try:
+        return current_user
+    except Exception as e:
+        func_logger.exception(e)
