d7f6f189a9ffbd7979098bbe77ad2438a5a02316
it-AVNG <it.avng@gmail.com>
2024-08-24 14:32:25 +0700

    add tests for logging features

---
+++ app/tests/test_logging.py
@@ -0,0 +1,25 @@
+import sys
+import os
+sys.path.append(f'{os.getcwd()}')
+import logging
+from logs.log_setup import get_log_config, log
+
+def test_log_config_exits():
+    '''assert that the config file is included'''
+    try:
+        get_log_config()
+    except Exception as e:
+        assert not isinstance(e,FileNotFoundError)
+
+def test_log_output(caplog):
+    '''assert that the log is created and write out'''
+    get_log_config()
+    caplog.set_level(logging.DEBUG)
+    @log
+    def run_test_function():
+        print('This is a test function')
+
+    run_test_function()
+
+    assert 'start run_test_function' in caplog.text
+    assert 'stop run_test_function' in caplog.text
